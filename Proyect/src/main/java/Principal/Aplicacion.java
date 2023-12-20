adc4b48b9abcb08d25ba6783e9ce6078eca830fd
and267 <148586602+and267@users.noreply.github.com>
2023-12-19 21:45:27 -0300

    Construcción clase Aplicación

--- Proyect/src/main/java/Principal/Aplicacion.java
+++ Proyect/src/main/java/Principal/Aplicacion.java
@@ -1,4 +1,9 @@
 package Principal;
 
+/** Este programa tiene como función principal la reserva de asientos de autobuses que cumplirán rutas predeterminadas*/
 public class Aplicacion {
+    public static void main(String[] args){
+        Ventana ventana = new Ventana();
+
+    }
 }
